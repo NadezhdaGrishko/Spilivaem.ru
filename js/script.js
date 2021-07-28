25796a65ab62b1b363d92d957312fc12c77d4fb8
NadezhdaGrishko <nadya.grishko.1999@gmail.com>
2021-07-28 16:36:11 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:12,
+    stagePadding:32,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        900:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
